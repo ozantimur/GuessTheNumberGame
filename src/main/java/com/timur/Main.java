aaf9542579176aaa68f881bc9801ef91ae0ca44d
Ozan Timur <ozan.timur2005@gmail.com>
2022-01-28 14:10:01 +0300

    added randomNumber method

--- src/main/java/com/timur/Main.java
+++ src/main/java/com/timur/Main.java
@@ -1,7 +1,16 @@
 package com.timur;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        
 
+    }
+
+    static int randomNumber() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100) + 1;
+        return randomNumber;
     }
 }
