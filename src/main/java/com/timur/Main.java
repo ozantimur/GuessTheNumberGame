cee00a650fb7b909357eb832f81350bb08903265
Ozan Timur <ozan.timur2005@gmail.com>
2022-01-28 14:43:44 +0300

    added while loop

--- src/main/java/com/timur/Main.java
+++ src/main/java/com/timur/Main.java
@@ -1,11 +1,35 @@
 package com.timur;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
 
+        Scanner scanner = new Scanner(System.in);
+        Integer randomNumber = randomNumber();
+
+        while (randomNumber.equals(randomNumber)) {
+            System.out.print("Guess: ");
+            Integer guess = scanner.nextInt();
+            if (guess.equals(randomNumber)) {
+                System.out.println("You guessed correct!!");
+                break;
+            } else if (guess > randomNumber) {
+                System.out.println("Guess smaller");
+                System.out.println("");
+                continue;
+            } else if (guess < randomNumber) {
+                System.out.println("Guess bigger");
+                System.out.println("");
+                continue;
+            } else {
+                System.out.println("Your guess is not valid");
+                System.out.println("Try again");
+                System.out.println("");
+                continue;
+            }
+        }
     }
 
     static int randomNumber() {
